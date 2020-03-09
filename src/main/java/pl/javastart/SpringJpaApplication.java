03dfc2825388bc1dd5f2d731de2ca5aa1895d69e
Tomasz Stepnik <tomasz.stepnik77@gmail.com>
2020-03-09 15:56:41 +0100

    try use addnotation @Table and @Column

--- src/main/java/pl/javastart/SpringJpaApplication.java
+++ src/main/java/pl/javastart/SpringJpaApplication.java
@@ -11,9 +11,13 @@ public class SpringJpaApplication {
 
     public static void main(String[] args) throws InterruptedException {
         ConfigurableApplicationContext ctx = SpringApplication.run(SpringJpaApplication.class, args);
+
         BookDao dao = ctx.getBean(BookDao.class);
         Book book = new Book("1234567890468", "Spring is so cool", "Javastart");
         dao.save(book);
+
+        Book foundBook = dao.get(1L);
+        System.out.println(foundBook);
         Thread.sleep(5000);
 
     }
--- src/main/java/pl/javastart/model/Book.java
+++ src/main/java/pl/javastart/model/Book.java
@@ -1,16 +1,16 @@
 package pl.javastart.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
+@Table(name = "books")
 public class Book implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue
+    @Column(name = "book_id")
     private Long id;
     private String isbn;
     private String title;
@@ -27,14 +27,11 @@ public class Book implements Serializable {
     public static long getSerialVersionUID() {
         return serialVersionUID;
     }
+    @Id
+    @GeneratedValue
+    public Long getId() { return id; }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public void setId(Long id) {this.id = id;}
 
     public String getIsbn() {
         return isbn;
@@ -44,9 +41,7 @@ public class Book implements Serializable {
         this.isbn = isbn;
     }
 
-    public String getTitle() {
-        return title;
-    }
+    public String getTitle() { return title; }
 
     public void setTitle(String title) {
         this.title = title;
